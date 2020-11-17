caa6cf1384f9d8b93effa19df8d5361cc5b863dd
Dmitriy Kovalenko <dmtr.kovalenko@outlook.com>
2020-11-17 20:18:46 +0200

    Paralellize tests

---
+++ cypress/integration/spec_copy.ts
@@ -0,0 +1,16 @@
+describe("Spec", () => {
+  it("Should click on the contacts page", () => {
+    cy.visit("https://fwdays.com/en/event/qa-fwdays-conference");
+
+    cy.clickOnHeaderLink("Contacts");
+  });
+
+  it.skip("Should click on facebook link", () => {
+    cy.visit("https://fwdays.com/en/event/qa-fwdays-conference");
+
+    cy.clickOnHeaderLink("Contacts");
+    cy.get(":nth-child(3) > .contact-info__link").within((el) => {
+      expect(el.attr("href")).to.contain("facebook.com");
+    });
+  });
+});
